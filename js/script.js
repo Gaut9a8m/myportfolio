3d4e47980b1cc878750e5206f879dce5bdf5af38
Gaut9a8m <gaut9a8m@gmail.com>
2018-03-12 21:26:23 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+    $(".php").circularProgress({
+        line_width: 6,
+        color: "#ccc",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "PHP"
+    }).circularProgress('animate', 90, 5000);
+});
+$(document).ready(function() {
+    $(".sql").circularProgress({
+        line_width: 6,
+        color: "#ccc",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "MYSQL"
+    }).circularProgress('animate', 99, 5000);
+});
+
+$(document).ready(function() {
+    $(".html").circularProgress({
+        line_width: 6,
+        color: "#ccc",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "HTML"
+    }).circularProgress('animate', 99, 5000);
+});
+
+$(document).ready(function() {
+    $(".css").circularProgress({
+        line_width: 6,
+        color: "#ccc",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "CSS"
+    }).circularProgress('animate', 95, 5000);
+});
+
+$(document).ready(function() {
+    $(".btstrap").circularProgress({
+        line_width: 6,
+        color: "#ccc",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "BOOTSTRAP"
+    }).circularProgress('animate', 89, 5000);
+});
+
+$(document).ready(function() {
+    $(".js").circularProgress({
+        line_width: 6,
+        color: "#fff",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "JS"
+    }).circularProgress('animate', 90, 5000);
+});
+
+$(document).ready(function() {
+    $(".jq").circularProgress({
+        line_width: 10,
+        color: "#fff",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "JQuery"
+    }).circularProgress('animate', 85, 5000);
+});
+
+$(document).ready(function() {
+    $(".java").circularProgress({
+        line_width: 10,
+        color: "#fff",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "JAVA"
+    }).circularProgress('animate', 95, 5000);
+});
+
